225c6015fee4cce6cf4fa89271946facdfe6220c
ADMIN\Treyten <treytencarey@gmail.com>
2018-10-15 11:46:56 -0500

    Added tile editor support for real-time online editing

--- world.lua
+++ world.lua
@@ -55,7 +55,9 @@ function removeTile(x, y, layer)
   local tileX, tileY = math.floor((x-gridX*gridSq.w)/16)+1, math.floor((y-gridY*gridSq.h)/16)+1
 
   if tiles[tostring(gridX)] ~= nil and tiles[tostring(gridX)][tostring(gridY)] ~= nil and tiles[tostring(gridX)][tostring(gridY)][tostring(layer)] ~= nil and tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)] ~= nil and tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)][tostring(tileY)] ~= nil then
-    tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)][tostring(tileY)]:remove()
+    local tile = tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)][tostring(tileY)]
+    tile:removeFromClients()
+    tile:remove()
     tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)][tostring(tileY)] = nil
   end
 end
@@ -72,13 +74,14 @@ function setTile(x, y, layer, img, cropX, cropY)
   if tile ~= nil then 
     tile:setImage(img); tile:crop(cropX, cropY, 16, 16)
   else
-    tile = CreateImage(img, (tileX-1)*16, (tileY-1)*16, 16, 16); tile:setClipped(false); tile:setScaled(parent:getScaled())
+    tile = CreateImage(img, (tileX-1)*16, (tileY-1)*16, 16, 16); tile:setID(getTileID(gridX, gridY, tileX, tileY, layer)); tile:setProperty("isTile", "1"); tile:setClipped(false); tile:setScaled(parent:getScaled())
     tile:crop(cropX,cropY,16,16)
 
     if tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)] == nil then tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)] = {}; end
     tiles[tostring(gridX)][tostring(gridY)][tostring(layer)][tostring(tileX)][tostring(tileY)] = tile
     layers[tostring(gridX)][tostring(gridY)][tostring(layer)]:addElement(tile)
   end
+  tile:sendToClients(true, "gridPos", tostring(gridX) .. "," .. tostring(gridY))
 
   -- print("Placing at: " .. tostring(gridX) .. ", " .. tostring(gridY) .. ", " .. tostring(tileX) .. ", " .. tostring(tileY))
 end
@@ -98,17 +101,28 @@ function reorderLayers(gridX, gridY)
   topLayer:bringToFront()
 end
 
+function getTileID(gridX, gridY, tileX, tileY, layer)
+  return "-" .. tostring(gridX) .. "-" .. tostring(gridY) .. "-" .. tostring(tileX) .. "-" .. tostring(tileY) .. "-" .. tostring(layer)
+end
+
 function loadGrid(gridX, gridY)
   checkCreateLists(gridX, gridY, 1)
   
   for x=0, gridSq.w-1, 16 do
     for y=0, gridSq.h-1, 16 do
-      local tile = CreateImage("Tilesets/tileset2.png", x, y, 16, 16); tile:setClipped(false); tile:setScaled(parent:getScaled())
-      tile:crop(0,0,16,16)
-      if tiles[tostring(gridX)][tostring(gridY)]["1"][tostring(x/16+1)] == nil then tiles[tostring(gridX)][tostring(gridY)]["1"][tostring(x/16+1)] = {}; end
-      tiles[tostring(gridX)][tostring(gridY)]["1"][tostring(x/16+1)][tostring(y/16+1)] = tile
-      -- Add tile to first layer of grid
-      layers[tostring(gridX)][tostring(gridY)]["1"]:addElement(tile)
+      local tileX, tileY = math.floor(x/16)+1, math.floor(y/16)+1
+      local ID = getTileID(gridX, gridY, tileX, tileY, 1)
+      local tile = game:getElementFromID(ID)
+      if tile == nil then
+        local tile = CreateImage("Tilesets/tileset2.png", x, y, 16, 16); tile:setID(ID); tile:setProperty("isTile", "1"); tile:setClipped(false); tile:setScaled(parent:getScaled())
+        tile:crop(0,0,16,16)
+        if tiles[tostring(gridX)][tostring(gridY)]["1"][tostring(x/16+1)] == nil then tiles[tostring(gridX)][tostring(gridY)]["1"][tostring(tileX)] = {}; end
+        tiles[tostring(gridX)][tostring(gridY)]["1"][tostring(tileX)][tostring(tileY)] = tile
+        -- Add tile to first layer of grid
+        layers[tostring(gridX)][tostring(gridY)]["1"]:addElement(tile)
+      else
+        layers[tostring(gridX)][tostring(gridY)]["1"]:addElement(tile)
+      end
     end
   end
   
@@ -139,6 +153,16 @@ function onWindowResize(lX, lY, lW, lH)
 
   local w, h = game:getWindowWidth(), game:getWindowHeight()
   parent:setPosition(parent:getX()+(w-lW)/2, parent:getY()+(h-lH)/2)
+end
+
+function onCommand(cmd, cmdStr)
+  local elem = server:getObjectFromCommand(cmd)
+  if elem ~= nil and elem:getProperty("isTile") == "1" and elem:getParent() == elem then
+    local tileProps = operations:getTokens(elem:getID(), "-")
+    local gridX, gridY, tileX, tileY, layer = tileProps[1], tileProps[2], tileProps[3], tileProps[4], tileProps[5]
+    checkCreateLists(gridX, gridY, layer)
+    layers[tostring(gridX)][tostring(gridY)][tostring(layer)]:addElement(elem)
+  end
 end
 
 function main()
